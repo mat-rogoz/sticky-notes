fe4acd5ca7bc86a20b0f30f485708dd28e2ea4e9
Mateusz Rogoz <mateusz.rogoz@pubnub.com>
2022-02-15 21:52:35 +0100

    Typeorm has been added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Sticky Notes
+
+Steps to run this project:
+
+1. Run `npm i` command
+2. Run `npm run start` command
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import express, { Request, Response } from 'express';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 import config from './../config/default';
+import logger from './utils/logger';
 
-const app = express()
-const port = config.port
+const app = express();
+const port = config.port;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.on('ready', () => {
+  app.listen(port, () => {
+    logger.info(`Sticky Notes app listening on port ${port}`);
+  });
+});
+
+createConnection()
+  .then(() => {
+    logger.info('Connected to database');
+    app.emit('ready');
+  })
+  .catch((err) => logger.error(err));
